90fdda7ba8d230a3b0e21a5d1110476dba2a26fc
Craig Parker <parkercm1984@yahoo.com>
2015-04-28 23:38:19 -0400

    Began adding javascript functions
    Made basic button function to input numbers into the input at top of
    calculator.  Only active on button number 1 currently.  Test successful
    will expand functionality soon

--- Calculator/public_html/script.js
+++ Calculator/public_html/script.js
@@ -4,3 +4,17 @@ Programmer: Craig Parker
  */
 
 
+
+
+function numberInput(){
+    var prevOutput = "";
+    var newOutput = "1";
+            
+    prevOutput = document.getElementById("calcOutput").value;
+    if (prevOutput === "0"){
+     document.getElementById("calcOutput").value = newOutput; 
+    }
+    else{
+    document.getElementById("calcOutput").value = prevOutput + newOutput ;
+    }
+};
